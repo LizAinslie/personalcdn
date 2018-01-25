559733cea949f1d788671157e501e125d626455e
Jellz <xdjellz@gmail.com>
2018-01-25 21:40:45 +1100

    optimise for jellyboi

--- cdn.js
+++ cdn.js
@@ -7,29 +7,25 @@ const app = express();
 const fs = require("fs");
 const randomstring = require("randomstring");
 const mime = require("mime-types");
-const hash = (/**
-    * Just a tiny trick for IntelliSense to work properly.
-    * @returns {String}
-    */()=>{return require("./hash.json");})();
+const auth = require('config.json').auth;
 const sha512 = require("sha512");
 const fileUpload = require("express-fileupload");
 try {fs.mkdirSync("data");}catch(error){}
 app.get("/", (req, res)=>{
-    res.send("Ron's personal CDN.");
+    res.send('Jellz CDN [for private use]');
 });
 app.use(express.static("data"));
 app.use(fileUpload({preserveExtension: true, safeFileNames: true}))
 app.post("/upload/:token", (req, res) => {
-    let token = sha512(req.params.token).toString("hex");
+    let token = req.params.token;
     let issue = null;
-    if (hash == token) {
+    if (auth == token) {
         if (!req.files) return res.send("no files");
         let v = req.files.file;
         if (!v) return res.send("no files file")
         let rdm = randomstring.generate(7);
         let ext = mime.extension(v.mimetype);
         if (!ext) return issue = "invalid mimetype.";
-        //TODO: maybe I should I implement something to stop self-XSSing.
         let fn = rdm+"."+ext;
         v.mv("data/"+fn)
         if (issue) return res.json({error:issue});
@@ -39,4 +35,4 @@ app.post("/upload/:token", (req, res) => {
     }
 });
 app.use(fileUpload({safeFileNames:true, preserveExtension:true}));
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3004);
